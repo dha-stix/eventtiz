017777031e86383d83d951b8508ca353162966cb
David Asaolu <asaoludavid234@yahoo.com>
2023-05-13 14:37:35 +0100

    🛠️: Remove React-Reveal animation.

--- components/HomeSection.js
+++ components/HomeSection.js
@@ -3,7 +3,6 @@ import createimage from "../images/createimage.svg";
 import event from "../images/event.svg";
 import share from "../images/share.svg";
 import Image from "next/image";
-import Fade from "react-reveal/Fade";
 
 const HomeSection = () => {
 	const array = [
@@ -32,30 +31,31 @@ const HomeSection = () => {
 			<h2 className='text-2xl text-[#ecbf66] mb-12'>How it works?</h2>
 			<div className='flex flex-col space-y-[50px] w-full py-4 md:px-[50px] px-[10px]'>
 				{array.map((item) => (
-					<Fade bottom key={item.id}>
-						<div className='bg-[#e1d1b3] shadow-md rounded-md w-full flex md:items-center md:justify-between p-10 md:flex-row flex-col'>
-							<div className='md:w-[60%] w-full flex md:flex-row flex-col items-center justify-center space-x-6'>
-								<div className='p-6 w-20 flex items-center justify-center h-20 border-gray-700 bg-[#C07F00] rounded-full mb-6 md:mb-auto'>
-									<h1 className='text-5xl'>{item.id}</h1>
-								</div>
-								<div className='md:w-1/2 w-full md:block flex flex-col items-center '>
-									<h2 className='md:text-3xl text-2xl mb-2  md:text-left  text-center font-bold'>
-										{item.title}
-									</h2>
-									<p className=' md:text-left text-center opacity-50'>
-										{item.content}
-									</p>
-								</div>
+					<div
+						className='bg-[#e1d1b3] shadow-md rounded-md w-full flex md:items-center md:justify-between p-10 md:flex-row flex-col'
+						key={item.id}
+					>
+						<div className='md:w-[60%] w-full flex md:flex-row flex-col items-center justify-center space-x-6'>
+							<div className='p-6 w-20 flex items-center justify-center h-20 border-gray-700 bg-[#C07F00] rounded-full mb-6 md:mb-auto'>
+								<h1 className='text-5xl'>{item.id}</h1>
 							</div>
-							<div className='md:w-[40%] w-full md:mt-auto mt-8'>
-								<Image
-									src={item.image}
-									alt='Create an Account'
-									className='w-full'
-								/>
+							<div className='md:w-1/2 w-full md:block flex flex-col items-center '>
+								<h2 className='md:text-3xl text-2xl mb-2  md:text-left  text-center font-bold'>
+									{item.title}
+								</h2>
+								<p className=' md:text-left text-center opacity-50'>
+									{item.content}
+								</p>
 							</div>
 						</div>
-					</Fade>
+						<div className='md:w-[40%] w-full md:mt-auto mt-8'>
+							<Image
+								src={item.image}
+								alt='Create an Account'
+								className='w-full'
+							/>
+						</div>
+					</div>
 				))}
 			</div>
 		</div>
